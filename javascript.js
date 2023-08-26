515c944d1c419f6ba0913fc93a8dee125d2f42fe
Anark <dreckmannmoises@gmail.com>
2023-08-26 01:47:04 -0300

    correcao pages

--- javascript.js
+++ javascript.js
@@ -5,28 +5,28 @@ const imgs = document.querySelectorAll('img')
 imgs[0].classList.add('item-active')
 imgs[0].style.opacity = '1'
 
-btnLeft.addEventListener('click', trocaImg)
-btnNext.addEventListener('click', trocaImg)
+btnLeft.addEventListener('click', trocaPrev)
+btnNext.addEventListener('click', trocaNext)
 
 let contador = 0
 
-function trocaImg(event) {
-    if(btnLeft) {
-        imgs[contador].classList.remove('item-active')
-        contador--
-        if (contador < 0) {
-            contador = imgs.length - 1
-        }
-        imgs[contador].classList.add('item-active')
-    } else {
-        imgs[contador].classList.remove('item-active')
-        contador++
-        if (contador >= imgs.length) {
-            contador = 0
-        }
-        imgs[contador].classList.add('item-active');
+function trocaPrev(event) {
+    imgs[contador].classList.remove('item-active')
+    contador--
+    if (contador < 0) {
+        contador = imgs.length - 1
     }
+    imgs[contador].classList.add('item-active')
     aplicarAnimacao(imgs[contador])
+}
+
+function trocaNext(event) {
+    imgs[contador].classList.remove('item-active')
+    contador++
+    if (contador >= imgs.length) {
+        contador = 0
+    }
+    imgs[contador].classList.add('item-active');
 }
 
 function aplicarAnimacao(element) {
